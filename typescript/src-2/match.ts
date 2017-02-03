4b54587d0c68844e86e6606b9d237d5fdacfd07f
Ryan Casperson <ryan.casperson@rackspace.com>
2017-02-02 21:41:21 -0600

    Make match into a simple data storage object

---
+++ typescript/src-2/match.ts
@@ -0,0 +1,26 @@
+import {Deck} from "./deck"
+
+class Match {
+    settings = null;
+    deck = new Deck()
+    round = {
+        number: 1,
+        trumpSuit: undefined,
+        bid: {
+            playerName: undefined,
+            amount: 0
+        }
+    };
+    trick = {
+        number: 1,
+        leadSuit: undefined,
+        cardsPlayed: {},
+        leadPlayer: undefined
+    };
+    teams = undefined
+    players = undefined
+
+    constructor(settings = undefined) {
+        this.settings = settings;
+    }
+}
