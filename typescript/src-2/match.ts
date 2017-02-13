479fd089aded9ff2d138aeffff47160d2d09a4f1
Ryan Casperson <ryan.casperson@rackspace.com>
2017-02-12 23:56:36 -0600

    Add some functionality onto the match class

--- typescript/src-2/match.ts
+++ typescript/src-2/match.ts
@@ -1,7 +1,11 @@
-import {Deck} from './deck'
+import * as _ from "lodash";
+
+import {Deck} from './deck';
+import {Team} from "./team";
+import {Player} from "./player";
 
 class Match {
-    settings = null;
+    settings;
     deck = new Deck()
     round = {
         number: 1,
@@ -17,10 +21,28 @@ class Match {
         cardsPlayed: {},
         leadPlayer: undefined
     };
-    teams = undefined
-    players = undefined
+    teams;
+    players;
 
     constructor(settings = undefined) {
         this.settings = settings;
+        this._assignTeamsAndPlayers(settings.teams)
+    }
+
+    get scores() {
+        let scores = {}
+        _.each(this.teams, (team, teamName) => {
+            scores[teamName] = team.score
+        })
+        return scores
+    }
+
+    _assignTeamsAndPlayers(teamSettings): void {
+        _.each(teamSettings, (playerNames, teamName) => {
+            this.teams[teamName] = new Team(teamName, playerNames)
+            _.each(playerNames, playerName => {
+                this.players[playerName] = new Player(playerName, teamName)
+            })
+        })
     }
 }
