5bed27d685bb035487ed744671af4f93d308918d
Ryan Casperson <ryan.casperson@rackspace.com>
2017-10-25 00:12:26 -0500

    Make manual bids right

--- typescript/ui/index.ts
+++ typescript/ui/index.ts
@@ -1,9 +1,5 @@
-import * as _ from 'lodash';
-
 import {Match} from '../engine/src/match';
-import {isValidBid} from '../engine/src/match/tools'
-
-import {getCardIDByName} from './helpers'
+import {MatchHandler} from './matchHandler';
 
 
 let settings = {
@@ -22,89 +18,6 @@ let settings = {
 }
 
 let match = new Match(settings)
+let matchHandler = new MatchHandler(match)
 
-function displayCards() {
-    let playersDiv = document.getElementById("players")
-    console.log(playersDiv.textContent)
-    _.each(match.players, (playerObject, playerName) => {
-        let playerDiv = document.createElement("div")
-
-        let h3 = document.createElement("h3")
-        let playerNameText = document.createTextNode(playerName)
-        h3.appendChild(playerNameText)
-        playerDiv.appendChild(h3)
-
-        let cardsDiv = document.createElement("div")
-        _.each(playerObject.hand, card => {
-            let cardDiv = document.createElement("div")
-            cardDiv.appendChild(document.createTextNode(card.name))
-            cardDiv.setAttribute("id", getCardIDByName(card))
-            cardsDiv.appendChild(cardDiv)
-        })
-        playerDiv.appendChild(cardsDiv)
-
-        playersDiv.appendChild(playerDiv)
-    })
-}
-
-let tableOrder = ["Player 1", "Player 3", "Player 2", "Player 4"]
-let currentDealerIndex = 0 // "Player 1" deals first
-let currentBidderIndex = 1
-let bids = []
-
-function requestBidFrom(playerName: string, match: Match) {
-    let actionDiv = document.getElementById("action")
-
-    let h3 = document.createElement("h3")
-    h3.appendChild(document.createTextNode(`Bid From ${playerName}:`))
-    actionDiv.appendChild(h3)
-
-    let validBids = _.filter([2, 3, 4], bid => {
-        return isValidBid(bid, match.round.bid, match.settings.maxBid)
-    })
-
-    let select = document.createElement("select")
-    select.id = "bidOptions"
-    select.options.add(new Option("pass", "pass", true, true))
-    _.each(validBids, validBid => {
-        let bid = validBid.toString()
-        select.options.add(new Option(bid, bid))
-    })
-    actionDiv.appendChild(select)
-
-    let makeBidButton = document.createElement("button")
-    makeBidButton.innerHTML = "Make Bid"
-    makeBidButton.addEventListener("click", () => {
-        let bidValue = select.options[select.selectedIndex].value;
-        console.log(bidValue)
-        bids.push(bidValue)
-        currentBidderIndex += 1
-        if (currentBidderIndex == tableOrder.length) {
-            currentBidderIndex = 0
-        }
-        match.makeBid(playerName, parseInt(bidValue))
-
-
-        while (actionDiv.hasChildNodes()) {
-            actionDiv.removeChild(actionDiv.lastChild);
-        }
-        console.log(bids.length)
-        if (bids.length < tableOrder.length) {
-            requestBidFrom(tableOrder[currentBidderIndex], match)
-        } else {
-            match.completeBidding()
-            let h4 = document.createElement("h4")
-            h4.innerHTML = `${match.round.bid.playerName} won the bidding with a bid of ${match.round.bid.amount}`
-            actionDiv.appendChild(h4)
-        }
-
-    })
-    actionDiv.appendChild(makeBidButton)
-
-}
-
-match.deal();
-
-displayCards()
-
-requestBidFrom(tableOrder[currentBidderIndex], match)
+matchHandler.start()
